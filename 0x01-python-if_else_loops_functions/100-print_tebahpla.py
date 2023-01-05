7d5e7e35fb4546b2f046a467959a8e77305a6fa7
renoxvi <vicreno08@gmail.com>
2023-01-05 15:12:45 -0800

    Loops and else

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a')-1, -1):
-    print('{:c}\n'.format(i) if i % 2 == 0 else chr(i-32), end='')
+    print('{:c}'.format(i) if i % 2 == 0 else chr(i-32), end='')
 
 # alternative method:
 # for i in range(0, 26):
--- 0x01-python-if_else_loops_functions/8-main.py
+++ 0x01-python-if_else_loops_functions/8-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 uppercase = __import__('8-uppercase').uppercase
 
-uppercase("holberton\n")
-uppercase("Holberton School 98 Battery street\n")
+uppercase("holberton")
+uppercase("Holberton School 98 Battery street")
