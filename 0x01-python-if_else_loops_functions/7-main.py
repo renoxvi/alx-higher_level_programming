76707c57f380d94c171904c2633550b20acb9e7f
renoxvi <vicreno08@gmail.com>
2023-01-05 15:07:42 -0800

    if_else_loops_functions

--- 0x01-python-if_else_loops_functions/7-main.py
+++ 0x01-python-if_else_loops_functions/7-main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 islower = __import__('7-islower').islower
 
-print("a is {}".format("lower" if islower("a") else "upper"))
-print("H is {}".format("lower" if islower("H") else "upper"))
-print("A is {}".format("lower" if islower("A") else "upper"))
-print("3 is {}".format("lower" if islower("3") else "upper"))
-print("g is {}".format("lower" if islower("g") else "upper"))
+print("a is {}\n".format("lower" if islower("a") else "upper"))
+print("H is {}\n".format("lower" if islower("H") else "upper"))
+print("A is {}\n".format("lower" if islower("A") else "upper"))
+print("3 is {}\n".format("lower" if islower("3") else "upper"))
+print("g is {}\n".format("lower" if islower("g") else "upper"))
