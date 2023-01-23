8780934ebb5b000b296d5097defd7f2b7e7e6801
renoxvi <vicreno08@gmail.com>
2023-01-23 09:44:19 -0800

    prints the first x elements of a list and only integers

--- 0x05-python-exceptions/2-main.py
+++ 0x05-python-exceptions/2-main.py
@@ -13,15 +13,3 @@ print("nb_print: {:d}".format(nb_print))
 
 nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
 print("nb_print: {:d}".format(nb_print))
-
-guillaume@ubuntu:~/0x05$ ./2-main.py
-12
-nb_print: 2
-12345
-nb_print: 5
-12345Traceback (most recent call last):
-  File "./2-main.py", line 14, in <module>
-    nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
-  File "/0x05/2-safe_print_list_integers.py", line 7, in safe_print_list_integers
-    print("{:d}".format(my_list[i]), end="")
-IndexError: list index out of range
